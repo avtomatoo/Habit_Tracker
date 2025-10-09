a2333d0bc435b09d5520c06785e866e40d50ded4
Sofia Pupovtseva <sonia.pupovtseva@yandex.ru>
2025-10-09 21:45:40 +0300

    Тест работы шрифтов и цветов. Добавление новых шрифтов.
    ! Удалены цвета из color.xml (была ошибка и инициализацией, хз короче)
    !Добавлены шрифты в папку Fonts, зареганы в MauiProgram.cs
    !Проверка с работй шрифтов пройдена, всё норм
    
    !!! При запуске приложения выходит ошибка "недопустимый синтаксис вызова статического метода "[System.IO.Path]::GetFileName(obj\Debug\net9.0-windows10.0.19041.0\win10-x64\resizetizer\f\Roboto-Italic-VariableFont_wdth,wght.ttf)". Не найден метод "System.IO.Path.GetFileName". Вызов статического метода должен иметь следующую форму: $([FullTypeName]::Method()), например $([System.IO.Path]::Combine(`a`, `b`)). Проверьте, что все параметры определены, имеют верный тип и указаны в нужном порядке."
    Надо проверить

--- Habit_Tracker/MauiProgram.cs
+++ Habit_Tracker/MauiProgram.cs
@@ -13,6 +13,12 @@ namespace Habit_Tracker
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+                    fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+                    fonts.AddFont("Roboto-Medium.ttf", "RobotoMedium");
+                    fonts.AddFont("Roboto-Light.ttf", "RobotoLight");
+                    fonts.AddFont("Roboto-VariableFont_wdth,wght.ttf", "RobotoVariableFontWdthWght");
+                    fonts.AddFont("Roboto-Italic-VariableFont_wdth,wght.ttf", "RobotoItalicVariableFontWdthWght");
                 });
 
 #if DEBUG
