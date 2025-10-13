e38ca85e388ea398c04b7f814fbe95a509d0d02d
Sofia Pupovtseva <sonia.pupovtseva@yandex.ru>
2025-10-13 23:38:34 +0300

    Revert "Удалены иконки из папки ресурсов. Добавлена новая страница "Создание привычек" + написан для неё код. SQLite работает и установлен. Добавлены классы Habit"
    This reverts commit 5bc72f1a28a5674ac357ff1e2ae1ca36acf02810.

--- Habit_Tracker/App.xaml.cs
+++ Habit_Tracker/App.xaml.cs
@@ -2,12 +2,14 @@
 {
     public partial class App : Application
     {
-        [Obsolete]
         public App()
         {
             InitializeComponent();
+        }
 
-            MainPage = new NavigationPage(new MainPage());
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
         }
     }
 }
